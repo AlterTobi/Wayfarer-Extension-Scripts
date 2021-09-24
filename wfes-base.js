8fbc6bd7a12a97d31f7e3035a5873e4190422dd5
Tobold Süßgras <fotofreund0815@gmail.com>
2021-09-25 00:22:26 +0200

    init

---
+++ wfes-base.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         WFES - Base
+// @namespace    https://gitlab.com/fotofreund0815/WFES
+// @version      0.0.1
+// @description  basic functionality for WFES
+// @author       AlterTobi
+// @match        https://wayfarer.nianticlabs.com/*
+// @downloadURL  https://gitlab.com/fotofreund0815/WFES
+// @grant        none
+// ==/UserScript==
