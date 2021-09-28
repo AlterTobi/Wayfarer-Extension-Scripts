6e4b20a1b6e008b4640a027c7ea447e676e4bf92
Tobold Süßgras <fotofreund0815@gmail.com>
2021-09-28 13:36:00 +0200

    move Repo to github

--- wfes-showcase.user.js
+++ wfes-showcase.user.js
@@ -4,8 +4,8 @@
 // @version      0.1.0
 // @description  Improve Wayfarer Showcase
 // @author       fotofreund0815
-// @match        https://wayfarer.nianticlabs.com/new/*
-// @downloadURL  https://gitlab.com/fotofreund0815/WFES/-/raw/main/wfes-showcase.user.js
+// @match        https://wayfarer.nianticlabs.com/*
+// @downloadURL  https://github.com/AlterTobi/WFES/raw/main/wfes-showcase.user.js
 // @grant        none
 // ==/UserScript==
 
