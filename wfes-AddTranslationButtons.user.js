123d41cc685d92be55c280f2912cb2707607e066
Tobold Süßgras <fotofreund0815@gmail.com>
2021-10-08 21:50:21 +0200

    add translate all to photoedits

--- wfes-AddTranslationButtons.user.js
+++ wfes-AddTranslationButtons.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         WFES - Add Translation Buttons
 // @namespace    https://gitlab.com/fotofreund0815/WFES
-// @version      0.1.5
+// @version      0.1.6
 // @description  Adds buttons to translate parts or all of the text associated with a wayspot
 // @author       MrJPGames / AlterTobi
 // @match        https://wayfarer.nianticlabs.com/*
@@ -44,6 +44,25 @@
         }
     }
 
+    function getTranslateAllButton(allText){
+        let translateButton = document.createElement("a");
+        translateButton.setAttribute("target", "wfesTranslate");
+        translateButton.setAttribute("class", "translateButton");
+        translateButton.setAttribute("style", "display: inline; color: black; background-image: none; width: fit-content;");
+        translateButton.href = "https://translate.google.com/?sl=auto&q=" + encodeURIComponent(allText);
+
+        let translateText = document.createElement("span");
+        translateText.innerText = "Translate all";
+
+        let translateImage = document.createElement("img");
+        translateImage.setAttribute("style", "height: 1.3em;");
+        translateImage.src = "data:image/svg+xml;charset=UTF-8,%3Csvg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'%3E%3Cpath d='M0 0h24v24H0z' fill='none'/%3E%3Cpath d='M12.87 15.07l-2.54-2.51.03-.03c1.74-1.94 2.98-4.17 3.71-6.53H17V4h-7V2H8v2H1v1.99h11.17C11.5 7.92 10.44 9.75 9 11.35 8.07 10.32 7.3 9.19 6.69 8h-2c.73 1.63 1.73 3.17 2.98 4.56l-5.09 5.02L4 19l5-5 3.11 3.11.76-2.04zM18.5 10h-2L12 22h2l1.12-3h4.75L21 22h2l-4.5-12zm-2.62 7l1.62-4.33L19.12 17h-3.24z'/%3E%3C/svg%3E";
+        translateButton.appendChild(translateImage);
+        translateButton.appendChild(translateText);
+
+        return translateButton;
+    }
+
     function addEditTranslationButtons(){
         addCSS();
         let elems = document.getElementsByClassName("poi-edit-text");
@@ -65,6 +84,8 @@
         console.warn('WFES: addPhotoTranslationButtons');
         addCSS();
         let elems = [];
+        let allText = "";
+
         elems.push(document.getElementsByClassName("text-lg")[0]);
         elems.push(document.getElementsByClassName("mt-2")[0]);
         for (let i = 0; i < elems.length; i++){
@@ -72,8 +93,17 @@
             translateButton.setAttribute("target", "wfesTranslate");
             translateButton.setAttribute("class", "translateButton");
             translateButton.href = "https://translate.google.com/?sl=auto&q=" + encodeURIComponent(elems[i].innerText);
+            
+            allText += elems[i].innerText + "\n\n";
+            
             elems[i].appendChild(translateButton);
         }
+
+        // translate all
+        let translateButton = getTranslateAllButton(allText);
+
+        let titleDiv = document.querySelector("app-review-photo > div > div.review-photo__info > div.flex.flex-col")
+        titleDiv.insertAdjacentElement('afterbegin', translateButton);
     }
     
     function addTranslationButtons(){
@@ -106,21 +136,7 @@
             document.getElementsByClassName("supporting-info-review-card flex-full xl:flex-1 ng-star-inserted")[0].getElementsByClassName("wf-review-card__body")[0].children[0].children[1].appendChild(translateButton);
         }
 
-        let translateButton = document.createElement("a");
-        translateButton.setAttribute("target", "wfesTranslate");
-        translateButton.setAttribute("class", "translateButton");
-        translateButton.setAttribute("style", "display: inline; color: black; background-image: none;");
-        translateButton.href = "https://translate.google.com/?sl=auto&q=" + encodeURIComponent(allText);
-
-        let translateText = document.createElement("span");
-        translateText.innerText = "Translate all";
-
-        let translateImage = document.createElement("img");
-        translateImage.setAttribute("style", "height: 1.3em;");
-        translateImage.src = "data:image/svg+xml;charset=UTF-8,%3Csvg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24'%3E%3Cpath d='M0 0h24v24H0z' fill='none'/%3E%3Cpath d='M12.87 15.07l-2.54-2.51.03-.03c1.74-1.94 2.98-4.17 3.71-6.53H17V4h-7V2H8v2H1v1.99h11.17C11.5 7.92 10.44 9.75 9 11.35 8.07 10.32 7.3 9.19 6.69 8h-2c.73 1.63 1.73 3.17 2.98 4.56l-5.09 5.02L4 19l5-5 3.11 3.11.76-2.04zM18.5 10h-2L12 22h2l1.12-3h4.75L21 22h2l-4.5-12zm-2.62 7l1.62-4.33L19.12 17h-3.24z'/%3E%3C/svg%3E";
-        translateButton.appendChild(translateImage);
-        translateButton.appendChild(translateText);
-
+        let translateButton = getTranslateAllButton(allText);
         let titleDiv = document.getElementById("title-description-card").children[0].children[0];
         titleDiv.appendChild(translateButton);
     }
