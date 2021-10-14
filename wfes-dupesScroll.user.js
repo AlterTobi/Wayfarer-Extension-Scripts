9c2966371ab5c97674906b774f7d3bd21998544d
Tobold Süßgras <fotofreund0815@gmail.com>
2021-10-14 20:23:30 +0200

    new Script: Dupes Scroll

---
+++ wfes-dupesScroll.user.js
@@ -0,0 +1,39 @@
+// ==UserScript==
+// @name         WFES - dupes Scroll
+// @namespace    https://github.com/AlterTobi/WFES/
+// @version      0.1.0
+// @description  WFES make duplicates strip scrollable by mouse wheel
+// @author       AlterTobi
+// @match        https://wayfarer.nianticlabs.com/*
+// @icon         https://wayfarer.nianticlabs.com/imgpub/favicon-256.png
+// @downloadURL  https://github.com/AlterTobi/WFES/raw/main/wfes-dupesScroll.user.js
+// @supportURL   https://github.com/AlterTobi/WFES/issues
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    function filmStripScroll() {
+        // Make film strip (duplicates) scrollable
+        function horizontalScroll(e) {
+            filmStripElem.scrollLeft += e.deltaY;
+            e.preventDefault(); // Stop regular scroll
+        }
+
+        let filmStripElem = document
+                .querySelector('#check-duplicates-card div.w-full.flex.overflow-x-auto.overflow-y-hidden.ng-star-inserted');
+
+        if (null === filmStripElem) {
+            setTimeout(filmStripScroll, 100);
+            return;
+        }
+
+        // Hook function to scroll event in filmstrip
+        filmStripElem.addEventListener("wheel", horizontalScroll, false);
+    }
+
+    window.addEventListener("WFESReviewPageNewLoaded", filmStripScroll);
+
+    console.log("WFES Script loaded: Dupes Scroll");
+})();
