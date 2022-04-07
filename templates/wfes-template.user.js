27653f26839267b630fd6ea66c65b1ee0b18cba3
Tobold Süßgras <fotofreund0815@gmail.com>
2022-04-07 23:26:00 +0200

    update teplate

--- templates/wfes-template.user.js
+++ templates/wfes-template.user.js
@@ -1,16 +1,7 @@
-// ==UserScript==
 // @name         WFES - Userscript
-// @namespace    https://github.com/AlterTobi/WFES/
-// @version      0.9.0
-// @description  WFES 
+// @version      0.9.1
+// @description  WFES Template 
 // @author       AlterTobi
-// @match        https://wayfarer.nianticlabs.com/*
-// @icon         https://wayfarer.nianticlabs.com/imgpub/favicon-256.png
-// @downloadURL  https://github.com/AlterTobi/WFES/raw/release/v0.9/wfes-template.user.js
-// @updateURL    https://github.com/AlterTobi/WFES/raw/release/v0.9/wfes-template.user.js
-// @supportURL   https://github.com/AlterTobi/WFES/issues
-// @grant        none
-// ==/UserScript==
 
 (function() {
     'use strict';
