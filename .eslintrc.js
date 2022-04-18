3f8bb7e90dfa5d4026ffd37d6acd754d3ca5ab30
Tobold Süßgras <fotofreund0815@gmail.com>
2022-04-18 23:24:13 +0200

    configure linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,14 +2,17 @@ module.exports = {
   env: {
     browser: true,
     es6: true,
+    greasemonkey: true
   },
   extends: [
-    'eslint:recommended',
+    "eslint:recommended",
   ],
   rules: {
     // General
-    'no-console': 'error',
-
+    // 'no-console': 'off',
+    "no-use-before-define": "error",
+    "space-before-function-paren": ["error", "never"],
+    
     /*
      * 'no-duplicate-imports': 'off', 'no-invalid-this': 'off', 'no-loop-func':
      * 'off', 'no-loss-of-precision': 'off', 'no-redeclare': 'off', 'no-shadow':
