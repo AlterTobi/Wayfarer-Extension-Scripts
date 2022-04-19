37da7e376039bc4b11ba44eacd99fcef64b758fd
Tobold Süßgras <fotofreund0815@gmail.com>
2022-04-19 11:37:53 +0200

    lint: allow smart tabs

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,13 +12,6 @@ module.exports = {
     // 'no-console': 'off',
     "no-use-before-define": "error",
     "space-before-function-paren": ["error", "never"],
-    
-    /*
-     * 'no-duplicate-imports': 'off', 'no-invalid-this': 'off', 'no-loop-func':
-     * 'off', 'no-loss-of-precision': 'off', 'no-redeclare': 'off', 'no-shadow':
-     * 'off', 'no-throw-literal': 'off', 'no-unused-expressions': 'off',
-     * 'no-return-await': 'off',
-     * 
-     */
+    "no-mixed-spaces-and-tabs": ["error","smart-tabs"]
   },
 };
