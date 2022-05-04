3bd413ebec7f447e1b9ee60be615f6c0673c8cb0
AlterTobi <AlterTobi@users.noreply.github.com>
2022-05-04 19:44:03 +0200

    Update ExpireTimer.user.js

--- _pages/ExpireTimer.user.js
+++ _pages/ExpireTimer.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name           WFES - Expire Timer
-// @version        1.0.4
+// @version        1.0.3
 // @description    Adds a simple timer to the top of the screen showing how much time you have left on the current review.
 // @author         MrJPGames / AlterTobi
 // @namespace      https://github.com/AlterTobi/WFES/
