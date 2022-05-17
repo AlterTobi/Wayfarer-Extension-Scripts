22fb44b02ae7c7016333d34225e8a63eb0397d2f
AlterTobi <fotofreund0815@gmail.com>
2022-05-17 17:57:10 +0200

    disable WF+ Dark Mode script

---
+++ no_support/wfes-DarkMode.js
@@ -0,0 +1,10 @@
+// @name         WF+ Dark Mode
+// @version      1.1.0
+// @description  WF+ Dark Mode - disabled
+// @author       AlterTobi
+
+(function() {
+  "use strict";
+  console.log("Script loaded:", GM_info.script.name, "v" + GM_info.script.version);
+  console.err("This script is no longer supported, please remove.");
+})();
