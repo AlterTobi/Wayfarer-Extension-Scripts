cfd27e82683f98a250035676ce906a953c7b17e7
Tobold Süßgras <fotofreund0815@gmail.com>
2022-08-21 21:55:58 +0200

    New script to mitigate Niatic scroll down bug

---
+++ no_support/wfes-scrollfix.js
@@ -0,0 +1,23 @@
+// @name         Scrollfix
+// @version      0.1.0
+// @description  Fix Scroll Down Bug
+// @author       AlterTobi
+
+(function() {
+  "use strict";
+
+  function scrollUp() {
+	const _dom = document.querySelector('mat-sidenav-content');
+    const _evfunc= () => {
+        console.log('event gescrollt');
+        document.querySelector('.wf-page-header__title > div:nth-child(1)').scrollIntoView();
+        _dom.removeEventListener('scroll',_evfunc);
+        };
+	_dom.addEventListener('scroll', _evfunc);
+  };
+
+  window.addEventListener("WFESReviewPageNewLoaded", scrollUp);
+
+  /* we are done :-) */
+  console.log("Script loaded:", GM_info.script.name, "v" + GM_info.script.version);
+})();
