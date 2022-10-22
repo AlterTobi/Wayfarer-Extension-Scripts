2101c73a5b3c1175b6e66e440f4e7d30cf2abfd7
AlterTobi <fotofreund0815@gmail.com>
2022-10-22 22:55:52 +0200

    ImageMod: full size image link also on nominations

--- wfes-ImageMod.js
+++ wfes-ImageMod.js
@@ -1,33 +1,23 @@
 // @name         image Mods
-// @version      1.0.1
+// @version      1.1.0
 // @description  open fullsize images in "named" tabs
 // @author       AlterTobi
 
 (function() {
   "use strict";
 
-  const myCssId = "imageModsCSS";
-  const myStyle = `.material-icons-fontsize {
-      font-size: 48px;
-    }
-    .lupe {
-      position: absolute;
-      left: 0px;
-    }
-    .bottom {
-      bottom: 0px;
-    }
-  `;
-
-  function addFullImageButton(elem, url, target, position = "afterEnd", styleclass = "lupe") {
+  function addFullImageButton(elem, url, target, position = "afterEnd", styleclass = "lupe", elemID=false, spanclass = "") {
     const a = document.createElement("a");
     const span = document.createElement("span");
 
-    span.className = "material-icons material-icons-fontsize";
+    span.className = "material-icons material-icons-fontsize " + spanclass;
     span.innerText = "search";
     a.appendChild(span);
     a.target = target;
     a.href = url;
+    if (elemID) {
+      a.id = elemID;
+    }
     a.className = styleclass;
     switch (position) {
     case "afterEnd":
@@ -42,7 +32,26 @@
     elem.insertAdjacentElement(position, a);
   }
 
-  function addFullSizeImageLinks() {
+  // setImage URL on existing element
+  function setImageURL(buttonID, imageUrl) {
+    const elem = document.getElementById(buttonID);
+    elem.href = imageUrl;
+  }
+
+  function addFullSizeImageLinksReview() {
+    const myCssId = "imageModsCSSReview";
+    const myStyle = `.material-icons-fontsize {
+        font-size: 48px;
+      }
+      .lupe {
+        position: absolute;
+        left: 0px;
+      }
+      .bottom {
+        bottom: 0px;
+      }
+    `;
+
     let elem, imageUrl;
     const myData = window.wfes.g.reviewPageData();
 
@@ -74,7 +83,55 @@
     }
   }
 
-  window.addEventListener("WFESReviewPageLoaded", () => {setTimeout(addFullSizeImageLinks, 100);});
+  function addFullSizeImageLinksNomDetail() {
+    const myCssId = "imageModsCSSNomDetail";
+    const myStyle = `.material-icons-fontsize {
+        font-size: 48px;
+      }
+      .lupesup {
+        position: absolute;
+        left: 0px;
+        top: 20px;
+      }
+      .luperel {
+        position: relative;
+        left: 0px;
+      }
+      .absolute {
+        position: absolute;
+      }
+    `;
+
+    const buttonIDmain = "imageModsBtnMain";
+    const buttonIDsup = "imageModsBtnSup";
+
+    let imageUrl;
+    const myData = window.wfes.g.nominationDetail();
+    window.wfes.f.addCSS(myCssId, myStyle);
+
+    const elem = document.getElementsByClassName("wf-image-modal details-pane__image");
+    imageUrl = myData.imageUrl + "=s0";
+
+    // main Image
+    if ( null === document.getElementById(buttonIDmain)) {
+      addFullImageButton(elem[0], imageUrl, "mainImage", "beforeBegin", "luperel", buttonIDmain, "absolute");
+    } else {
+      setImageURL(buttonIDmain, imageUrl);
+    }
+
+    // Supporting Image
+    if (myData.supportingImageUrl) {
+      imageUrl = myData.supportingImageUrl + "=s0";
+      if ( null === document.getElementById(buttonIDsup)) {
+        addFullImageButton(elem[1].parentNode, imageUrl, "supportingImage", "beforeEnd", "lupesup", buttonIDsup);
+      } else {
+        setImageURL(buttonIDsup, imageUrl);
+      }
+    }
+  }
+
+  window.addEventListener("WFESNominationDetailLoaded", () => {setTimeout(addFullSizeImageLinksNomDetail, 100);});
+  window.addEventListener("WFESReviewPageLoaded", () => {setTimeout(addFullSizeImageLinksReview, 100);});
 
   console.log("Script loaded:", GM_info.script.name, "v" + GM_info.script.version);
 })();
