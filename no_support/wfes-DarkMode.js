1de3b2e03fcbb2ee3b62f6b9e416c0f0439a024c
Tobold Süßgras <fotofreund0815@gmail.com>
2023-03-06 21:56:10 +0100

    lint

--- no_support/wfes-DarkMode.js
+++ no_support/wfes-DarkMode.js
@@ -6,6 +6,6 @@
 (function() {
   "use strict";
   console.log("Script loaded:", GM_info.script.name, "v" + GM_info.script.version);
-  console.error(GM_info.script.name,": This script is no longer supported, please remove.");
+  console.error(GM_info.script.name, ": This script is no longer supported, please remove.");
   window.alert(GM_info.script.name + "\nThis script is no longer supported, please remove.");
 })();
