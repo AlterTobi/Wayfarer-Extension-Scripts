4c088cce6079990b13fcc8c633ca9966325dfae5
Tobold Süßgras <fotofreund0815@gmail.com>
2023-03-08 00:04:17 +0100

    eigene Klasse nutzen für Höhe

--- no_support/wfes-reviewEnlargeCards.js
+++ no_support/wfes-reviewEnlargeCards.js
@@ -7,11 +7,17 @@
   "use strict";
 
   const myCssId = "enlargeCardsCSS";
-  const myStyle = ".wf-review-card { height: 490px !important; }";
+  const myStyle = ".wfes-h490 { min-height: 490px; }";
+  const cardSelectors = ["app-should-be-wayspot > wf-review-card", "app-title-and-description > wf-review-card", "app-supporting-info > wf-review-card"];
 
-  window.addEventListener("WFESReviewPageLoaded", () => {
+  function enlargeCards() {
     window.wfes.f.addCSS(myCssId, myStyle);
-  });
+    cardSelectors.forEach(selector => {
+      window.wfes.f.waitForElem(selector).then((elem)=>{elem.classList.add("wfes-h490");});
+    });
+  }
+
+  window.addEventListener("WFESReviewPageLoaded", enlargeCards);
 
   console.log("Script loaded:", GM_info.script.name, "v" + GM_info.script.version);
 })();
