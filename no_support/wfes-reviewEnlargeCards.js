5eab14375ac34bae0f117649cf34612323f177f9
AlterTobi <AlterTobi@users.noreply.github.com>
2023-03-08 12:54:55 +0100

    Hotfix/wf5 2 (#67)

--- no_support/wfes-reviewEnlargeCards.js
+++ no_support/wfes-reviewEnlargeCards.js
@@ -1,5 +1,5 @@
 // @name Review Enlarge Cards
-// @version 1.0.0
+// @version 1.0.1
 // @description fix for small heightin Wayfarer 5.2
 // @author AlterTobi
 
@@ -7,14 +7,16 @@
   "use strict";
 
   const myCssId = "enlargeCardsCSS";
-  const myStyle = ".wfes-h490 { min-height: 490px; }";
+  const myStyle = ".wfes-h490 { min-height: 490px; } .wfes-h790 { min-height: 790px; }";
   const cardSelectors = ["app-should-be-wayspot > wf-review-card", "app-title-and-description > wf-review-card", "app-supporting-info > wf-review-card"];
+  const dupeSelector = "#check-duplicates-card";
 
   function enlargeCards() {
     window.wfes.f.addCSS(myCssId, myStyle);
     cardSelectors.forEach(selector => {
       window.wfes.f.waitForElem(selector).then((elem)=>{elem.classList.add("wfes-h490");});
     });
+    window.wfes.f.waitForElem(dupeSelector).then((elem)=>{elem.classList.add("wfes-h790");});
   }
 
   window.addEventListener("WFESReviewPageLoaded", enlargeCards);
