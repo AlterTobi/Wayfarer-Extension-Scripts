d39d35d8307146f82b5e4c555bb0e10b36bd97bb
Tobold Süßgras <fotofreund0815@gmail.com>
2023-03-12 22:31:39 +0100

    Enlarge Cards is outdated

--- no_support/wfes-reviewEnlargeCards.js
+++ no_support/wfes-reviewEnlargeCards.js
@@ -7,21 +7,7 @@
 
 (function() {
   "use strict";
-
-  const myCssId = "enlargeCardsCSS";
-  const myStyle = ".wfes-h490 { min-height: 490px; } .wfes-h790 { min-height: 790px; }";
-  const cardSelectors = ["app-should-be-wayspot > wf-review-card", "app-title-and-description > wf-review-card", "app-supporting-info > wf-review-card"];
-  const dupeSelector = "#check-duplicates-card";
-
-  function enlargeCards() {
-    window.wfes.f.addCSS(myCssId, myStyle);
-    cardSelectors.forEach(selector => {
-      window.wfes.f.waitForElem(selector).then((elem)=>{elem.classList.add("wfes-h490");});
-    });
-    window.wfes.f.waitForElem(dupeSelector).then((elem)=>{elem.classList.add("wfes-h790");});
-  }
-
-  window.addEventListener("WFESReviewPageLoaded", enlargeCards);
-
   console.log("Script loaded:", GM_info.script.name, "v" + GM_info.script.version);
+  console.error(GM_info.script.name, ": This script is no longer supported, please remove.");
+//  window.alert(GM_info.script.name + "\nThis script is no longer supported, please remove.");
 })();
