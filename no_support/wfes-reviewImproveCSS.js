2b3aa5fd122f15655ba3f016089fd6b786d2e2d5
Tobold Süßgras <fotofreund0815@gmail.com>
2023-03-12 22:27:54 +0100

    remove texts

--- no_support/wfes-reviewImproveCSS.js
+++ no_support/wfes-reviewImproveCSS.js
@@ -7,7 +7,13 @@
   "use strict";
 
   const myCssId = "enlargeCardsCSS";
-  const myStyle = ".wfes-h490 { min-height: 490px; } .wfes-h790 { min-height: 790px; }";
+  const myStyle = `
+    .wfes-h490 { min-height: 490px; }
+    .wfes-h790 { min-height: 790px; }
+    .wfes-none { display: none; }
+    `;
+  
+  
   const cardSelectors = ["app-should-be-wayspot > wf-review-card", "app-title-and-description > wf-review-card", "app-supporting-info > wf-review-card"];
   const dupeSelector = "#check-duplicates-card";
 
@@ -15,6 +21,9 @@
     window.wfes.f.addCSS(myCssId, myStyle);
     cardSelectors.forEach(selector => {
       window.wfes.f.waitForElem(selector).then((elem)=>{elem.classList.add("wfes-h490");});
+      // remove description texts
+      const seltext = selector + " div:nth-child(1) > div";
+      window.wfes.f.waitForElem(seltext).then((elem)=>{elem.classList.add("wfes-none");});
     });
     window.wfes.f.waitForElem(dupeSelector).then((elem)=>{elem.classList.add("wfes-h790");});
   }
