f3027be877d58629567b07f52db84ab9c972528d
AlterTobi <AlterTobi@users.noreply.github.com>
2023-04-14 11:36:06 +0200

    Update wfes-reviewImproveCSS.meta.js

--- dev/wfes-reviewImproveCSS.meta.js
+++ dev/wfes-reviewImproveCSS.meta.js
@@ -7,8 +7,8 @@
 // @homepage       https://altertobi.github.io/Wayfarer-Extension-Scripts/
 // @supportURL     https://github.com/AlterTobi/Wayfarer-Extension-Scripts/issues
 // @icon           https://wayfarer.nianticlabs.com/imgpub/favicon-256.png
-// @downloadURL    https://altertobi.github.io/Wayfarer-Extension-Scripts/dev/wfes-reviewImproveCSS.user.js
-// @updateURL      https://altertobi.github.io/Wayfarer-Extension-Scripts/dev/wfes-reviewImproveCSS.meta.js
+// @downloadURL    https://altertobi.github.io/Wayfarer-Extension-Scripts/wfes-reviewImproveCSS.user.js
+// @updateURL      https://altertobi.github.io/Wayfarer-Extension-Scripts/wfes-reviewImproveCSS.meta.js
 // @match          https://wayfarer.nianticlabs.com/*
 // @grant          none
 // ==/UserScript==
