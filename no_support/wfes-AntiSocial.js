efc346dfec60efeb97b0bd6fb96df88fe6537ef8
AlterTobi <AlterTobi@users.noreply.github.com>
2023-12-26 15:57:06 +0100

    new: Anti-social
    by special request from @Tntnnbltn from WDD

---
+++ no_support/wfes-AntiSocial.js
@@ -0,0 +1,23 @@
+// @name         Anti Social
+// @version      1.0.0
+// @description  hides group size selection from socialize card (greetings to @Tntnnbltn)
+// @author       AlterTobi
+
+(function() {
+  "use strict";
+
+  const myCssId = "antiSocialCSS";
+  const myStyle = `div#socialize-card > div:nth-child(2) {
+    display: none;
+    }
+    `;
+
+  function antiSocial() {
+    window.wfes.f.addCSS(myCssId, myStyle);
+  }
+
+  window.addEventListener("WFESReviewPageNewLoaded", antiSocial);
+
+  /* we are done :-) */
+  console.log("Script loaded:", GM_info.script.name, "v" + GM_info.script.version);
+})();
