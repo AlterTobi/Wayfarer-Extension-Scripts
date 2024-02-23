8f33fb60e60f5affd32c353f47e698140a0083b4
AlterTobi <AlterTobi@users.noreply.github.com>
2024-02-23 22:57:45 +0100

    new: disableTextDiff

---
+++ no_support/wfes-disableTextDiff.js
@@ -0,0 +1,28 @@
+// @name         disableTextDiff
+// @version      1.0.0
+// @description  disables the Niantic text diff display by clicking at the slider
+// @author       AlterTobi
+
+(function() {
+  "use strict";
+
+  const matSlider = "mat-slide-toggle";
+  const inputSlider = "input.mat-slide-toggle-input";
+
+  function disableTextDiff() {
+    window.wfes.f.waitForElem(matSlider).then( () =>{
+      const sliders = document.querySelectorAll(inputSlider);
+      sliders.forEach((s) => {
+        if ("true" === s.getAttribute("aria-checked")) {
+          s.click();
+        }
+      });
+    })
+    .catch();
+  }
+
+  window.addEventListener("WFESReviewPageEditLoaded", disableTextDiff);
+
+  /* we are done :-) */
+  console.log("Script loaded:", GM_info.script.name, "v" + GM_info.script.version);
+})();
