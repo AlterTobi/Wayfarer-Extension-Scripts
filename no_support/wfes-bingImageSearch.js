816238d8a82586bc796467b5110525358183951f
AlterTobi <AlterTobi@users.noreply.github.com>
2024-03-09 17:36:52 +0100

    testfile bing reverse image search

---
+++ no_support/wfes-bingImageSearch.js
@@ -0,0 +1,56 @@
+// @name         Reverse Image Search Bing
+// @version      1.0.0
+// @description  Search Bing for Images
+// @author       AlterTobi
+
+(function() {
+  "use strict";
+
+/*
+  const myCssId = "bingImageSearchCSS";
+  const myStyle = `body {
+    }
+    `;
+*/
+  const mainImageElem = "wf-image-modal flex-grow bg-contain bg-center bg-no-repeat";
+  const targetName = "wfesReverseImageBing";
+
+  function prepareURI(imgUrl) {
+    const encoded = encodeURIComponent(imgUrl);
+    const bingURI = `https://www.bing.com/images/search?view=detailv2&iss=sbi&form=SBIWEB&sbisrc=UrlPaste&q=imgurl:${encoded}&idpbck=1&selectedindex=0&id=${encoded}&ccid=wKrWr9BS&mediaurl=${encoded}&exph=2815&expw=2592&vt=2&sim=1`;
+    return bingURI;
+  }
+
+  function addLink(url, elem ) {
+    const span = document.createElement("span");
+    const link = document.createElement("a");
+    link.href = url;
+    link.target = targetName;
+    link.textContent = "Bing Reverse Image Search";
+    span.appendChild(link);
+    elem.parentNode.insertBefore(span, elem);
+  }
+
+  function reviewNew() {
+  //  window.wfes.f.addCSS(myCssId, myStyle);
+    const data = window.wfes.g.reviewPageData();
+    // wait for main photo
+    window.wfes.f.waitForElem(mainImageElem)
+      .then((e)=>{
+        const imageUrl = data.imageUrl + "=s0";
+        addLink(prepareURI(imageUrl), e);
+      })
+      .catch((e) => {console.warn(GM_info.script.name, ": ", e);});
+  }
+
+  function reviewPhoto() {
+    // window.wfes.f.addCSS(myCssId, myStyle);
+    // nothing yet
+  }
+
+  window.addEventListener("WFESReviewPageNewLoaded", reviewNew);
+  window.addEventListener("WFESReviewPagePhotoLoaded", reviewPhoto);
+
+  /* we are done :-) */
+  console.log("Script loaded:", GM_info.script.name, "v" + GM_info.script.version);
+})();
