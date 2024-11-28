dc3ea7da28d7990de1c69aa9538b6d0e94444bab
AlterTobi <AlterTobi@users.noreply.github.com>
2024-11-28 12:45:48 +0100

    review debug: warnung ausgeben

--- obsolete/wfes-reviewDebug.js
+++ obsolete/wfes-reviewDebug.js
@@ -9,4 +9,7 @@
   "use strict";
   /* we are done :-) */
   console.log("Script loaded:", GM_info.script.name, "v" + GM_info.script.version);
+  console.error(GM_info.script.name, ": This script is no longer supported, please remove.");
+  window.alert(GM_info.script.name + "\nThis script is no longer supported, please remove.");
+
 })();
