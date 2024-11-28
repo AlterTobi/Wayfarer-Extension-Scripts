4190cfa2bedcfb21a4460bccd27f466eac2ee73c
AlterTobi <AlterTobi@users.noreply.github.com>
2024-11-28 11:59:51 +0100

    review debug wird nicht mehr gebraucht

---
+++ obsolete/wfes-reviewDebug.js
@@ -0,0 +1,12 @@
+// @name         review Debug
+// @version      1.1.9
+// @description  show some debugging info
+// @author       AlterTobi
+// @downloadURL    https://altertobi.github.io/Wayfarer-Extension-Scripts/wfes-Debug.user.js
+// @updateURL      https://altertobi.github.io/Wayfarer-Extension-Scripts/wfes-Debug.meta.js
+
+(function() {
+  "use strict";
+  /* we are done :-) */
+  console.log("Script loaded:", GM_info.script.name, "v" + GM_info.script.version);
+})();
