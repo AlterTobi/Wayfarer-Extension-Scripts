a8f2ade1059f03ba5dbf39a1c5c8bfdf442910b8
AlterTobi <AlterTobi@users.noreply.github.com>
2025-02-11 23:28:20 +0100

    now longer show appeal statement
    as Wayfarer does this now

--- wfes-AppealData.js
+++ wfes-AppealData.js
@@ -1,14 +1,34 @@
 // @name         Appeal Data
-// @version      1.3.0
+// @version      1.4.0
 // @description  save and show appeal your statements
 // @author       AlterTobi
 
 (function() {
   "use strict";
 
+  const sessvarMiss = "warnBase";
+  const baseMinVersion = "2.3.0";
   const lStoreList = "wfes_AppealData";
+  const warnFlag = "wfes_AppealData_warn";
   const myID = "nominationAppealData";
   const nominationSelector = "app-details-pane > div > div > div > div.flex.flex-row.justify-between";
+
+  function warnOnce() {
+    window.wfes.f.localGet(warnFlag, false).then((warn)=>{
+      if(warn) {
+        return(false);
+      } else {
+        const msg = "INFO: " + GM_info.script.name + "wayfarer now handles appeal statements " +
+                "this script will still save data, but wills not show it in the future (as long as Wayfarer does this) " +
+                "Maybe you want to disable the script?";
+        window.wfes.f.createNotification(msg, "fuchsia");
+        window.wfes.f.localSave(warnFlag, true);
+        return(true);
+      }
+    })
+      .catch((e) => {console.warn(GM_info.script.name, ": ", e);});
+    return(false);
+  }
 
   function storeAppealData() {
     const appeal = window.wfes.g.reviewAppeal();
@@ -67,8 +87,26 @@
 
   }
 
-  window.addEventListener("WFESReviewAppealSent", storeAppealData);
-  window.addEventListener("WFESNominationDetailLoaded", NominationSelected);
+  const init = () => {
+    window.addEventListener("WFESReviewAppealSent", storeAppealData);
+    // Wayfarer 5.24 shows user statement, no need to display duplicate content
+    if (warnOnce()) {window.addEventListener("WFESNominationDetailLoaded", NominationSelected);}
+  };
 
+  // === no changes needed below this line ======================
+  if("undefined" === typeof(wfes)) {
+    if (undefined === sessionStorage[sessvarMiss]) {
+      sessionStorage[sessvarMiss] = 1;
+      alert("Missing WFES Base. Please install from https://altertobi.github.io/Wayfarer-Extension-Scripts/");
+      console.error("Missing WFES Base. Please install from https://altertobi.github.io/Wayfarer-Extension-Scripts/");
+    }
+  } else if (window.wfes.f.hasMinVersion(baseMinVersion)) {
+    init();
+  } else {
+    console.warn(GM_info.script.name, "Need at least wfes-Base version ", baseMinVersion, " Please upgrade.");
+  }
+
+  /* we are done :-) */
   console.log("Script loaded:", GM_info.script.name, "v" + GM_info.script.version);
+
 })();
