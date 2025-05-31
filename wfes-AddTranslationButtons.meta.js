820a7656d15addb578c0d6208848259632dc424d
AlterTobi <AlterTobi@users.noreply.github.com>
2025-05-31 23:09:18 +0000

    Deploying to gh-pages from @ AlterTobi/Wayfarer-Extension-Scripts@9697ed785bae9e1ff6c42405a979276a84ad3e90 🚀

---
+++ wfes-AddTranslationButtons.meta.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name           WFES - Add Translation Buttons
+// @version        2.3.2
+// @description    Adds a button to translate the text associated with a wayspot
+// @author         AlterTobi
+// @match          https://wayfarer.nianticlabs.com/*
+// @match          https://www.deepl.com/*
+// @match          https://translate.kagi.com/*
+// @namespace      https://github.com/AlterTobi/WFES/
+// @homepage       https://altertobi.github.io/Wayfarer-Extension-Scripts/
+// @supportURL     https://github.com/AlterTobi/Wayfarer-Extension-Scripts/issues
+// @icon           https://altertobi.github.io/Wayfarer-Extension-Scripts/assets/icon_32.png
+// @icon64         https://altertobi.github.io/Wayfarer-Extension-Scripts/assets/icon_64.png
+// @downloadURL    https://altertobi.github.io/Wayfarer-Extension-Scripts/wfes-AddTranslationButtons.user.js
+// @updateURL      https://altertobi.github.io/Wayfarer-Extension-Scripts/wfes-AddTranslationButtons.meta.js
+// @match          https://wayfarer.nianticlabs.com/*
+// @grant          none
+// ==/UserScript==
+
