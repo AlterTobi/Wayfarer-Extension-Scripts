06332acdaea6ade16be304fca9270c46baf98ff3
AlterTobi <AlterTobi@users.noreply.github.com>
2025-06-01 13:29:16 +0000

    Deploying to gh-pages from @ AlterTobi/Wayfarer-Extension-Scripts@c95c675c3a3e87330444f25f21852f418cee7e64 🚀

---
+++ dev/wfes-dupesScroll.meta.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name           WFES - dupes Scroll
+// @version        1.1.3
+// @description    make duplicates strip scrollable by mouse wheel
+// @author         AlterTobi
+// @namespace      https://github.com/AlterTobi/WFES/
+// @homepage       https://altertobi.github.io/Wayfarer-Extension-Scripts/
+// @supportURL     https://github.com/AlterTobi/Wayfarer-Extension-Scripts/issues
+// @icon           https://altertobi.github.io/Wayfarer-Extension-Scripts/dev/assets/icon_32.png
+// @icon64         https://altertobi.github.io/Wayfarer-Extension-Scripts/dev/assets/icon_64.png
+// @downloadURL    https://altertobi.github.io/Wayfarer-Extension-Scripts/dev/wfes-dupesScroll.user.js
+// @updateURL      https://altertobi.github.io/Wayfarer-Extension-Scripts/dev/wfes-dupesScroll.meta.js
+// @match          https://wayfarer.nianticlabs.com/*
+// @grant          none
+// ==/UserScript==
+
