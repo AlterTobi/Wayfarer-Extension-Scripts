a48b517cec0d25a63508ca0c2cd142798b70a987
AlterTobi <AlterTobi@users.noreply.github.com>
2025-07-30 19:42:14 +0000

    Deploying to gh-pages from @ AlterTobi/Wayfarer-Extension-Scripts@d553c73f7b08d2ecb893f7b49d369e65e928a5ae 🚀

---
+++ dev/wfes-NominationDetail.meta.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name           WFES - Nomination Detail
+// @version        1.0.4
+// @description    improvements for nomination detail page
+// @author         AlterTobi
+// @namespace      https://github.com/AlterTobi/WFES/
+// @homepage       https://altertobi.github.io/Wayfarer-Extension-Scripts/
+// @supportURL     https://github.com/AlterTobi/Wayfarer-Extension-Scripts/issues
+// @icon           https://altertobi.github.io/Wayfarer-Extension-Scripts/dev/assets/icon_32.png
+// @icon64         https://altertobi.github.io/Wayfarer-Extension-Scripts/dev/assets/icon_64.png
+// @downloadURL    https://altertobi.github.io/Wayfarer-Extension-Scripts/dev/wfes-NominationDetail.user.js
+// @updateURL      https://altertobi.github.io/Wayfarer-Extension-Scripts/dev/wfes-NominationDetail.meta.js
+// @match          https://wayfarer.nianticlabs.com/*
+// @grant          none
+// ==/UserScript==
+
