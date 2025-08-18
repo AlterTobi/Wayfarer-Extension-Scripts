5ae066dab25486e6c52b79f4f1ba0fe432cdac1e
AlterTobi <AlterTobi@users.noreply.github.com>
2025-08-18 19:24:20 +0000

    Deploying to gh-pages from @ AlterTobi/Wayfarer-Extension-Scripts@16e5ed9b9772f0e98ddea6e9ed90b30582b041c6 🚀

---
+++ dev/wfes-reviewDebug.meta.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name           WFES - review Debug
+// @version        1.1.10
+// @description    show some debugging info - obsolete
+// @author         AlterTobi
+// @downloadURL    https://altertobi.github.io/Wayfarer-Extension-Scripts/wfes-Debug.user.js
+// @updateURL      https://altertobi.github.io/Wayfarer-Extension-Scripts/wfes-Debug.meta.js
+// @namespace      https://github.com/AlterTobi/WFES/
+// @homepage       https://altertobi.github.io/Wayfarer-Extension-Scripts/
+// @supportURL     https://github.com/AlterTobi/Wayfarer-Extension-Scripts/issues
+// @icon           https://altertobi.github.io/Wayfarer-Extension-Scripts/dev/assets/icon_32.png
+// @icon64         https://altertobi.github.io/Wayfarer-Extension-Scripts/dev/assets/icon_64.png
+// @match          https://wayfarer.nianticlabs.com/*
+// @grant          none
+// ==/UserScript==
+
---
+++ wfes-AutoHold.meta.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name           WFES - AutoHold
+// @version        1.3.1
+// @description    put nomination on HOLD when additional stament contains the text "#hold"
+// @author         AlterTobi
+// @namespace      https://github.com/AlterTobi/WFES/
+// @homepage       https://altertobi.github.io/Wayfarer-Extension-Scripts/
+// @supportURL     https://github.com/AlterTobi/Wayfarer-Extension-Scripts/issues
+// @icon           https://altertobi.github.io/Wayfarer-Extension-Scripts/assets/icon_32.png
+// @icon64         https://altertobi.github.io/Wayfarer-Extension-Scripts/assets/icon_64.png
+// @downloadURL    https://altertobi.github.io/Wayfarer-Extension-Scripts/wfes-AutoHold.user.js
+// @updateURL      https://altertobi.github.io/Wayfarer-Extension-Scripts/wfes-AutoHold.meta.js
+// @match          https://wayfarer.nianticlabs.com/*
+// @grant          none
+// ==/UserScript==
+
