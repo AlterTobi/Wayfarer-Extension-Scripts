540f7ff11dba7338091082ea6a52ef7337e8620b
AlterTobi <AlterTobi@users.noreply.github.com>
2025-09-27 23:31:58 +0200

    add date to obsolete files

--- obsolete/wfes-reviewAddStreetAddress.js
+++ obsolete/wfes-reviewAddStreetAddress.js
@@ -8,5 +8,5 @@
   console.log("Script loaded:", GM_info.script.name, "v" + GM_info.script.version);
   console.error(GM_info.script.name, ": This script is no longer supported, please remove.");
   window.alert(GM_info.script.name + "\nThis script is no longer supported, please remove.");
-
+  // 2025-09-27
 })();
