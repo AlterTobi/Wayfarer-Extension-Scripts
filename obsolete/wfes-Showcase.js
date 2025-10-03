deb4fcdb97e4e4f232f14815acebcc2d97abfcdb
AlterTobi <AlterTobi@users.noreply.github.com>
2025-10-03 13:26:12 +0200

    show obsolete waring for showcase script

--- obsolete/wfes-Showcase.js
+++ obsolete/wfes-Showcase.js
@@ -1,5 +1,5 @@
 // @name         Showcase
-// @version      1.4.0
+// @version      1.4.1
 // @description  Improve Wayfarer Showcase - obsolete
 // @author       AlterTobi
 
@@ -8,6 +8,7 @@
 
   console.log("Script loaded:", GM_info.script.name, "v" + GM_info.script.version);
   console.error(GM_info.script.name, ": This script is no longer supported, please remove.");
+  window.wfes.f.createNotification(`${GM_info.script.name} This script is no longer supported, please remove`, "red" );
   //   window.alert(GM_info.script.name + "\nThis script is no longer supported, please remove.");
-  // 2025-09-27
+  // 2025-10-03
 })();
