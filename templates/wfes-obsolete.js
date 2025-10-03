5e59f3bb53ecd546bda95611983e7fd8166d93cb
AlterTobi <AlterTobi@users.noreply.github.com>
2025-10-03 13:30:35 +0200

    new template: obsolete

---
+++ templates/wfes-obsolete.js
@@ -0,0 +1,16 @@
+// @name         Obsolete
+// @version      1.0.0
+// @description  Obsolete Template
+// @author       AlterTobi
+
+(function() {
+  "use strict";
+
+  const warnText = ": This script is no longer supported, please remove.";
+
+  console.log("Script loaded:", GM_info.script.name, "v" + GM_info.script.version);
+  console.error(GM_info.script.name, warnText);
+  window.wfes.f.createNotification(`${GM_info.script.name} ${warnText}`, "red" );
+  window.alert(GM_info.script.name + "\n" + warnText);
+  // 2025-10-03
+})();
