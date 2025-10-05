8e1066306547f3fa08e19ad36ae106c460db9d9b
AlterTobi <AlterTobi@users.noreply.github.com>
2025-10-05 19:23:04 +0200

    move semi-colon from warntext to output

--- templates/wfes-obsolete.js
+++ templates/wfes-obsolete.js
@@ -6,11 +6,11 @@
 (function() {
   "use strict";
 
-  const warnText = ": This script is no longer supported, please uninstall.";
+  const warnText = "This script is no longer supported, please uninstall.";
 
   console.log("Script loaded:", GM_info.script.name, "v" + GM_info.script.version);
-  console.error(GM_info.script.name, warnText);
-  window.wfes.f.createNotification(`${GM_info.script.name} ${warnText}`, "red" );
+  console.error(GM_info.script.name, ":", warnText);
+  window.wfes.f.createNotification(`${GM_info.script.name}: ${warnText}`, "red" );
   window.alert(GM_info.script.name + "\n" + warnText);
   // 2025-10-03
 })();
