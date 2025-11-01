809b33e15aee36db0859bc9da5002f12080c5c05
AlterTobi <AlterTobi@users.noreply.github.com>
2025-11-01 23:35:29 +0000

    Deploying to gh-pages from @ AlterTobi/Wayfarer-Extension-Scripts@cfd47370b7772e240862affd480cd07785f493c7 🚀

---
+++ dev/wfes-ClickEnlargeImages.meta.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name           WFES - Click Enlarge Images
+// @version        1.0.0
+// @description    auto-click the enlarge images symbols (requires image mod script)
+// @author         AlterTobi
+// @namespace      https://github.com/AlterTobi/WFES/
+// @homepage       https://altertobi.github.io/Wayfarer-Extension-Scripts/
+// @supportURL     https://github.com/AlterTobi/Wayfarer-Extension-Scripts/issues
+// @icon           https://altertobi.github.io/Wayfarer-Extension-Scripts/dev/assets/icon_32.png
+// @icon64         https://altertobi.github.io/Wayfarer-Extension-Scripts/dev/assets/icon_64.png
+// @downloadURL    https://altertobi.github.io/Wayfarer-Extension-Scripts/dev/wfes-ClickEnlargeImages.user.js
+// @updateURL      https://altertobi.github.io/Wayfarer-Extension-Scripts/dev/wfes-ClickEnlargeImages.meta.js
+// @match          https://wayfarer.nianticlabs.com/*
+// @grant          none
+// ==/UserScript==
+
