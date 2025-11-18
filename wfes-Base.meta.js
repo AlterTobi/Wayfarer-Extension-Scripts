bce440065b721429ac14e56fbaa9df874f1938f1
AlterTobi <AlterTobi@users.noreply.github.com>
2025-11-18 22:06:29 +0000

    Deploying to gh-pages from @ AlterTobi/Wayfarer-Extension-Scripts@8b00414d29d3c960b21ebd0a238a94a9b4375cff 🚀

---
+++ wfes-Base.meta.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name           WFES - Base
+// @version        2.6.0
+// @description    basic functionality for WFES
+// @author         AlterTobi
+// @run-at         document-start
+// @namespace      https://github.com/AlterTobi/WFES/
+// @homepage       https://altertobi.github.io/Wayfarer-Extension-Scripts/
+// @supportURL     https://github.com/AlterTobi/Wayfarer-Extension-Scripts/issues
+// @icon           https://altertobi.github.io/Wayfarer-Extension-Scripts/assets/icon_32.png
+// @icon64         https://altertobi.github.io/Wayfarer-Extension-Scripts/assets/icon_64.png
+// @downloadURL    https://altertobi.github.io/Wayfarer-Extension-Scripts/wfes-Base.user.js
+// @updateURL      https://altertobi.github.io/Wayfarer-Extension-Scripts/wfes-Base.meta.js
+// @match          https://wayfarer.nianticlabs.com/*
+// @grant          none
+// ==/UserScript==
+
