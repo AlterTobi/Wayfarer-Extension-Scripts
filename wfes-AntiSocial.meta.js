1a57f3b924b88faa273f62e543da02ec3f7556bb
AlterTobi <AlterTobi@users.noreply.github.com>
2026-03-23 19:07:44 +0000

    Deploying to gh-pages from @ AlterTobi/Wayfarer-Extension-Scripts@010c1252c6da7e6e8f898bf4669fdc8ffca84beb 🚀

---
+++ wfes-AntiSocial.meta.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name           WFES - Anti Social
+// @version        1.0.1
+// @description    hides group size selection from socialize card (greetings to @Tntnnbltn)
+// @author         AlterTobi
+// @namespace      https://github.com/AlterTobi/WFES/
+// @homepage       https://altertobi.github.io/Wayfarer-Extension-Scripts/
+// @supportURL     https://github.com/AlterTobi/Wayfarer-Extension-Scripts/issues
+// @icon           https://altertobi.github.io/Wayfarer-Extension-Scripts/assets/icon_32.png
+// @icon64         https://altertobi.github.io/Wayfarer-Extension-Scripts/assets/icon_64.png
+// @downloadURL    https://altertobi.github.io/Wayfarer-Extension-Scripts/wfes-AntiSocial.user.js
+// @updateURL      https://altertobi.github.io/Wayfarer-Extension-Scripts/wfes-AntiSocial.meta.js
+// @match          https://wayfarer.nianticlabs.com/*
+// @grant          none
+// ==/UserScript==
+
---
+++ wfes-Showcase.meta.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name           WFES - Showcase
+// @version        1.5.0
+// @description    Improve Wayfarer Showcase
+// @author         AlterTobi
+// @namespace      https://github.com/AlterTobi/WFES/
+// @homepage       https://altertobi.github.io/Wayfarer-Extension-Scripts/
+// @supportURL     https://github.com/AlterTobi/Wayfarer-Extension-Scripts/issues
+// @icon           https://altertobi.github.io/Wayfarer-Extension-Scripts/assets/icon_32.png
+// @icon64         https://altertobi.github.io/Wayfarer-Extension-Scripts/assets/icon_64.png
+// @downloadURL    https://altertobi.github.io/Wayfarer-Extension-Scripts/wfes-Showcase.user.js
+// @updateURL      https://altertobi.github.io/Wayfarer-Extension-Scripts/wfes-Showcase.meta.js
+// @match          https://wayfarer.nianticlabs.com/*
+// @grant          none
+// ==/UserScript==
+
