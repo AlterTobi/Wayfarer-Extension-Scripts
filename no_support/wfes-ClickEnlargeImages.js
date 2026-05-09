e805d3c888d20a2868f1aa1a200afb7e322071c2
AlterTobi <fotofreund0815@gmail.com>
2026-05-09 21:08:46 +0200

    clickenlarge auch anpassen

--- no_support/wfes-ClickEnlargeImages.js
+++ no_support/wfes-ClickEnlargeImages.js
@@ -7,7 +7,7 @@
   "use strict";
 
   const l1Sel = "app-photo-b > wf-review-card-b > div.wf-review-card__body > div > a.lupe";
-  const l2Sel = "wf-image-carousel > a.lupesup";
+  const l2Sel = "div#lupesup";
 
   function click() {
     window.wfes.f.waitForElem(l2Sel).then((elem)=>{ elem.click(); })
